61bd3fba7733d935025e3f514876a26d6c23a636
zchen225 <zac41@pitt.edu>
2024-11-18 19:23:35 -0500

    Client sends packets to server, very rudimentary.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,28 +1,26 @@
 use std::net::UdpSocket;
 
-use bevy::render::render_resource::Buffer;
-
-fn main()  {
-
+fn main() {
     println!("Starting Server");
-    
-    let socket = UdpSocket::bind("127.0.0.1:4000")
-        .unwrap();
+
+    let socket = UdpSocket::bind("127.0.0.1:4000").unwrap();
 
     println!("Server listening on {}", socket.local_addr().unwrap());
 
     let mut buffer = [0; 1024];
 
     loop {
-        let (size, source) = socket.recv_from(&mut buffer).unwrap();
+        let result = socket.recv_from(&mut buffer);
+        match result {
+            Ok((size, source)) => {
+                println!("Received {} bytes from {}", size, source);
+                let request = String::from_utf8_lossy(&buffer[..size]);
 
-        let request = String::from_utf8_lossy(&buffer[..size]);
-
-        println!("Received {} from {}", request, source);
-
-        let response = "Hello from Server...!";
-
-        socket.send_to(response.as_bytes(), source)
-            .unwrap();
+                println!("Received {} from {}", request, source);
+            }
+            Err(e) => {
+                eprintln!("Failed to receive data: {}", e);
+            }
+        }
     }
 }
