b8d3ad6cdb00e9d30b7f6ee38bf1354901f268e6
amil10 <andrewamilcar@gmail.com>
2024-12-08 07:00:30 -0500

    psudeo random spawning and whirlpool functionality

--- project_code/src/whirlpool/mod.rs
+++ project_code/src/whirlpool/mod.rs
@@ -6,13 +6,23 @@ mod systems;
 use crate::GameState;
 use crate::GameworldState;
 use systems::*;
+use crate::whirlpool::components::Whirlpool;
 
 pub struct WhirlpoolPlugin;
 
 pub fn setup_whirlpool_cooldown(mut commands: Commands) {
     commands.insert_resource(WhirlpoolCooldownTimer {
-        timer: Timer::from_seconds(2.0, TimerMode::Once), // 2 second cooldown
+        timer: Timer::from_seconds(0.032, TimerMode::Once), 
     });
+}
+
+fn cleanup_whirlpools(
+    mut commands: Commands,
+    whirlpool_query: Query<Entity, With<Whirlpool>>,
+) {
+    for entity in whirlpool_query.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
 }
 
 impl Plugin for WhirlpoolPlugin {
@@ -34,6 +44,7 @@ impl Plugin for WhirlpoolPlugin {
                     check_whirlpool_collisions,
                 )
                     .run_if(in_state(GameworldState::Ocean))
-            );
+            )
+            .add_systems(OnExit(GameworldState::Ocean), cleanup_whirlpools);
     }
 }
